c8a9f9c8bb4515248c01dbcd32688ec298260d65
Rafael Armelin <raffael.armelin@gmail.com>
2021-07-15 15:29:24 +0100

    reviewd

--- scr/index.js
+++ scr/index.js
@@ -2,10 +2,10 @@
 const namesList = ["Mason","Marcora","Rico", "Neves", "Ivanov"];
 
 //- Have a prompt that adds your last name to the array
-let surnameP = prompt("Please enter your surname");
-console.log(surnameP);
+let surname = prompt("Please enter your surname");
+console.log(surname);
 
-namesList.push(surnameP);
+namesList.push(surname);
 console.log(namesList);
 
 
@@ -13,29 +13,29 @@ console.log(namesList);
 const alphabeticalNames = namesList.sort()
 console.log("sortedLastNames: " , alphabeticalNames);
 
-let positionOfMyLastName = 0;
+let positionOfMySurname = null;
 
 //- Have an output that tells you what is the position of your name in the sorted array
 
-for (let i = 0; i < namesList.length; i++) {
-        console.log("your name " + namesList[i] + " is in the position", [i]);
-}
+for (let i = 0; i < alphabeticalNames.length; i++) {
+        const surnameToCheck = alphabeticalNames[i];
 
-// Other solution?
-//const namePosition = namesList.indexOf(surnameP); 
+        if (surnameToCheck === "Armelin"){
+                positionOfMySurname = i;
+        }
+}
+console.log("The position of my surname is: ", positionOfMySurname);
 
 
 //- Have an output that Uppercase's all names
 
-for (let i = 0; i < namesList.length; i++) {
-    const upperCaseNames = namesList[i].toUpperCase();
-        console.log("upperCaseLastNames:  " + upperCaseNames );
+const upperCaseLastNames = [];
+
+for (let i = 0; i < alphabeticalNames.length; i++) {
+        const surname = alphabeticalNames[i];
+        const surnameInUpperCase = surname.toUpperCase();
+
+        upperCaseLastNames.push(surnameInUpperCase);
 }
+console.log("Surnames in uppercase:  ", upperCaseLastNames);
 
-
-/*Let's take it a step further and group each sorted name with its upper-cased one in a sub-array,
- which in its turn will go inside another array to group it all together.
-Output the result into the console.*/
-
-console.log(alphabeticalNames);
-console.log(upperCaseNames);
